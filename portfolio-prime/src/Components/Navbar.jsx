cbe4f0fb15b4f5ebddefab41153cb78201fe55e1
Ultimamaximus <82238915+Ultimamaximus@users.noreply.github.com>
2024-08-11 14:05:47 -0700

    I added styling to the Navbar.

--- portfolio-prime/src/Components/Navbar.jsx
+++ portfolio-prime/src/Components/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import './Navbar.css'; 
 
-const Navbar = ({ setPage }) => {
+const Navbar = () => {
   return (
-    <nav style={styles.navbar}>
-        <div style={styles.logoContainer}>
-            <img src={logoSrc} alt={logoAlt} style={styles.logo} />
-        </div>
-      <ul style={styles.navList}>
-        <li style={styles.navItem}>
-          <button style={styles.navLink} onClick={() => setPage('about')}>About</button>
+    <nav className="navbar">
+      <div className="logo-container">
+        <h2>Victor Branson</h2>
+      </div>
+      <ul className="nav-list">
+        <li className="nav-item">
+          <button className="nav-link">About</button>
         </li>
-        <li style={styles.navItem}>
-          <button style={styles.navLink} onClick={() => setPage('work')}>Work</button>
+        <li className="nav-item">
+          <button className="nav-link">Work</button>
         </li>
-        <li style={styles.navItem}>
-          <button style={styles.navLink} onClick={() => setPage('contact')}>Contact</button>
+        <li className="nav-item">
+          <button className="nav-link">Contact</button>
         </li>
       </ul>
     </nav>
