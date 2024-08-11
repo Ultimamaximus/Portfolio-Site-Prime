b1a3e80dd647e5cb0e96c04e535d4ce576ab657f
Ultimamaximus <82238915+Ultimamaximus@users.noreply.github.com>
2024-08-11 14:57:07 -0700

    I created and styled the hero component.

--- portfolio-prime/src/Components/Hero.jsx
+++ portfolio-prime/src/Components/Hero.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Hero.css';
+import heroImage from '../Images/test-img.webp'; // Adjust the path as needed
 
 const Hero = () => {
   return (
     <div className="hero-container">
+      <img src={heroImage} alt="Hero" className="hero-image" />
       <div className="text-container">
-        <h1 className="title">Welcome to My Website</h1>
-        <p className="subtitle">Building the future, one line of code at a time</p>
+        <h1 className="title">howdy folks, I'm</h1>
+        <p className="subtitle">Victor Branson</p>
       </div>
     </div>
   );
