51d7ccc49bd49310aaa58509adb47da905bacbf4
Ultimamaximus <82238915+Ultimamaximus@users.noreply.github.com>
2024-08-15 18:14:26 -0700

    I added animations that appear on initial load.

--- portfolio-prime/src/Components/Hero.jsx
+++ portfolio-prime/src/Components/Hero.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './Hero.css';
+import 'animate.css';
 import heroImage from '../Images/test-img.webp'; // Adjust the path as needed
 
 const Hero = () => {
-  return (
-    <div className="hero-container">
-      <img src={heroImage} alt="Hero" className="hero-image" />
-      <div className="text-container">
-        <h1 className="title">hello folks, I'm</h1>
-        <p className="subtitle">Victor Branson</p>
+    return (
+      <div className="hero-container">
+        <img src={heroImage} alt="Hero" className="hero-image" />
+        <div className="text-container">
+          <h1 className="title animate__animated animate__fadeInUp">hello folks, I'm</h1>
+          <p className="subtitle animate__animated animate__fadeInUp">Victor Branson</p>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };  
 
 export default Hero;
--- portfolio-prime/src/Components/Navbar.jsx
+++ portfolio-prime/src/Components/Navbar.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
+import 'animate.css';
 import './Navbar.css'; 
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="logo-container">
-        <h2>Victor Branson</h2>
-      </div>
-      <ul className="nav-list">
-        <li className="nav-item">
-          <button className="nav-link">About</button>
-        </li>
-        <li className="nav-item">
-          <button className="nav-link">Work</button>
-        </li>
-        <li className="nav-item">
-          <button className="nav-link">Contact</button>
-        </li>
-      </ul>
-    </nav>
-  );
-};
-
-export default Navbar;
+    return (
+      <nav className="navbar animate__animated animate__fadeInDown">
+        <div className="logo-container">
+          <h2>Victor Branson</h2>
+        </div>
+        <ul className="nav-list">
+          <li className="nav-item">
+            <button className="nav-link">About</button>
+          </li>
+          <li className="nav-item">
+            <button className="nav-link">Work</button>
+          </li>
+          <li className="nav-item">
+            <button className="nav-link">Contact</button>
+          </li>
+        </ul>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
+  
